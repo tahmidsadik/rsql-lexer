342670c3ec3dd1a26d6735253eefaf79115eea4f
Tahmid Sadik <tahmidsadik112@gmail.com>
2020-04-18 23:58:56 +0600

    Adds lexer vocabularies[WIP]

---
+++ src/vocabulary.rs
@@ -0,0 +1,11 @@
+const 
+    SELECT = "select",
+    CREATE = "create",
+    INSERT = "insert",
+    UPDATE = "update",
+    DELETE = "delete",
+    DROP = "drop",
+    FROM = "from",
+    INTO = "into";
+
+
