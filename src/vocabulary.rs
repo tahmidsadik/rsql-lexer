fb4172189022a229e0a55bb2a701cd8cd55ba9aa
Tahmid Sadik <tahmidsadik112@gmail.com>
2020-04-19 17:21:09 +0600

    Adds lexer vocabularies[WIP]

--- src/vocabulary.rs
+++ src/vocabulary.rs
@@ -1,11 +1,31 @@
-const 
-    SELECT = "select",
-    CREATE = "create",
-    INSERT = "insert",
-    UPDATE = "update",
-    DELETE = "delete",
-    DROP = "drop",
-    FROM = "from",
-    INTO = "into";
+const SELECT: string = "select";
+const CREATE: string = "create";
+const INSERT: string = "insert";
+const UPDATE: string = "update";
+const DELETE: string = "delete";
+const DROP: string = "drop";
+const FROM: string = "from";
+const INTO: string = "into";
+const WHERE: string = "where";
+const DISTINCT: string = "distinct";
+const ALTER: string = "alter";
+const ADD: string = "add";
+const SET: string = "set";
+const TRUNCATE: string = "truncate";
+const AS: string = "as";
+const ASC: string = "asc";
+const DESC: string = "desc";
+const BETWEEN: string = "between";
+const HAVING: string = "having";
+const IN: string = "in";
+const JOIN: string = "join";
+const EXISTS: string = "exists";
+const LIKE: string = "like";
+const CASE: string = "case";
 
-
+pub fn get_keywords() -> Vec<string> {
+    return Vec![
+        SELECT, CREATE, INSERT, UPDATE, DELETE, DROP, FROM, INTO, WHERE, DISTINCT, ALTER, ADD, SET,
+        ASC, DESC, BETWEEN, HAVING, IN, JOIN, EXISTS, LIKE, CASE
+    ];
+}
